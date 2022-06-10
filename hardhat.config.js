5c206a0cd550a754b842d911420754f70062aadc
Krish-bhardwaj <krishbhardwaj27203@gmail.com>
2022-06-10 16:07:30 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+// TEST NET LOCAL
+// require("@nomiclabs/hardhat-waffle");
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// // You need to export an object to set up your config
+// // Go to https://hardhat.org/config/ to learn more
+
+// /**
+//  * @type import('hardhat/config').HardhatUserConfig
+//  */
+// module.exports = {
+//   solidity: "0.8.1",
+// };
+
+// RINKIBIE TEST NET
+require('@nomiclabs/hardhat-waffle');
+require("dotenv").config({ path: ".env" });
+
+module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
